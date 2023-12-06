7b59ece3b460ac4b93f6607ee83216500140f28d
Elaine <elainewang125@gmail.com>
2023-12-06 13:09:54 -0500

    created pairs represented by numbers

--- src/main.rs
+++ src/main.rs
@@ -82,85 +82,67 @@ fn process_route_file(path: &str) -> Result<HashSet<(String, String)>, Box<dyn E
 }
 
 
+fn map_airports_to_integers(
+    routes: &HashSet<(String, String)>,
+) -> (HashMap<String, usize>, Vec<(usize, usize)>) {
+    let mut airport_to_int = HashMap::new();
+    let mut int_routes = Vec::new();
+    let mut next_int = 0;
 
+    for (src, dst) in routes {
+        let src_int = *airport_to_int.entry(src.clone()).or_insert_with(|| {
+            let current = next_int;
+            next_int += 1;
+            current
+        });
+        let dst_int = *airport_to_int.entry(dst.clone()).or_insert_with(|| {
+            let current = next_int;
+            next_int += 1;
+            current
+        });
 
-fn convert_to_numeric(
-    airport_data: &HashMap<String, String>,
-    route_data: &HashSet<(String, String)>,
-
-) -> (BTreeMap<String, usize>, HashSet<(usize, usize)>) {
-
-    // Create a mapping of airport names to unique numbers
-    let mut airport_name_to_number = BTreeMap::new();
-    let mut current_number = 0;
-    for (code, name) in airport_data.iter() {
-        airport_name_to_number
-            .entry(code.clone())
-            .or_insert_with(|| {
-                current_number += 1;
-                current_number - 1
-            });
-        airport_name_to_number
-            .entry(name.clone())
-            .or_insert_with(|| {
-                current_number += 1;
-                current_number - 1
-            });
+        int_routes.push((src_int, dst_int));
     }
 
-    // Convert airport pairs to numeric representation
-    let numeric_route_data: HashSet<(usize, usize)> = route_data
-        .iter()
-        .map(|(source, dest)| {
-            (
-                *airport_name_to_number.get(source).unwrap(),
-                *airport_name_to_number.get(dest).unwrap(),
-            )
-        })
-        .collect();
-
-    (airport_name_to_number, numeric_route_data)
+    (airport_to_int, int_routes)
 }
 
+
+fn map_integers_to_airport_names(
+    airport_to_int: &HashMap<String, usize>,
+    airport_code_to_name: &HashMap<String, String>,
+) -> HashMap<usize, String> {
+    let mut int_to_airport_name = HashMap::new();
+
+    for (code, &int) in airport_to_int {
+        if let Some(name) = airport_code_to_name.get(code) {
+            int_to_airport_name.insert(int, name.clone());
+        }
+    }
+
+    int_to_airport_name
+}
+
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let airport_data = process_airports_file("airports.csv")?;
-    let route_data = process_route_file("routes.csv")?;
 
-    // Convert airport names to numeric representation
-    let (airport_name_to_number, numeric_route_data) =
-        convert_to_numeric(&airport_data, &route_data);
+    let routes = process_route_file("routes.csv")?;
+    let airport_code_to_name = process_airports_file("airports.csv")?;
+    println!("Airport codes to names: {:?}", airport_code_to_name);
+    
+    let (airport_to_int, int_routes) = map_airports_to_integers(&routes);
+    println!("Airport to integer mapping: {:?}", airport_to_int);
+    println!("Integer routes: {:?}", int_routes);
+    
+    let int_to_airport_name = map_integers_to_airport_names(&airport_to_int, &airport_code_to_name);
+    println!("Integer to airport name mapping: {:?}", int_to_airport_name);
 
-    println!("Airport Name to Number: {:?}", airport_name_to_number);
-    println!("Numeric Route Data: {:?}", numeric_route_data);
+    // // Create the graph
+    // let numeric_edges: graph::ListOfEdges = numeric_route_data.iter().map(|&pair| pair).collect();
+    // let graph = Graph::create_directed(airport_name_to_number.len(), &numeric_edges);
 
-    // Create the graph
-    let numeric_edges: graph::ListOfEdges = numeric_route_data.iter().map(|&pair| pair).collect();
-    let graph = Graph::create_directed(airport_name_to_number.len(), &numeric_edges);
-
-    // Print the graph
-    println!("Graph: {:?}", graph);
+    // // Print the graph
+    // println!("Graph: {:?}", graph);
 
     Ok(())
 }
-
-
-
-// fn main() -> Result<(), Box<dyn Error>> {
-//     // read two files
-//     let airport_data = process_airports_file("airports.csv")?;
-//     let route_data = process_route_file("routes.csv")?;
-
-//     println!("Airport Data: {:?}", airport_data);
-//     println!("Route Data: {:?}", route_data);
-
-//     // Convert pairs of routes to numbers
-//     let route_data_numbers = convert_to_numbers(&route_data, &airport_data);
-//     println!("Route Data Numbers: {:?}", route_data_numbers);
-
-//     // Create graph
-//     let route_data_numbers: Vec<_> = route_data_numbers.iter().cloned().collect();
-//     let graph = Graph::create_directed(airport_data.len(), &route_data_numbers);
-
-
-//     Ok(())
-// }
